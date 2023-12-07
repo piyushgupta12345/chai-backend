596cbed3d9ac2a9f7749f1ae6243cc581c767d20
Piyush Gupta <124340622+piyushgupta12345@users.noreply.github.com>
2023-12-07 21:04:33 +0530

    userModel, videoModel and jwtToken

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 // require('dotenv').config({path: './env'})
 
 import connectDB  from "./db/index.js";
+import dotenv from 'dotenv';
+import app from './app.js';
 
+dotenv.config({
+    path: './env'
+})
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8080, ()=>{
+        console.log(`server is running at the port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed !!! ", err);
+})
